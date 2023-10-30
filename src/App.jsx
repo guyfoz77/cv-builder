818ad3ced9ad9487caa085f58bb8570d24056659
Guy <125807123+guyfoz77@users.noreply.github.com>
2023-10-30 15:49:03 +0000

    Build basic App structure

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,31 @@ function App() {
 
   return (
     <>
-      
+      <aside className="userInput">
+        <div className="clearLoad"> 
+        {/* todo */}
+        </div>
+        <div className="personalDetails">
+        {/* todo */}
+        </div>
+        <div className="education">
+        {/* todo */}
+        </div>
+        <div className="experience">
+        {/* todo */}
+        </div>
+      </aside>
+      <main className="CV">
+        <div className="personalDetailsCV">
+        {/* todo */}
+        </div>
+        <div className="educationCV">
+        {/* todo */}
+        </div>
+        <div className="professionalExperienceCV">
+        {/* todo */}
+        </div>
+      </main>
     </>
   );
 }
