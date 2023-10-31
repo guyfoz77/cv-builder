c77fb808ecc955b81a5dd1a6f24be082ae8c5484
Guy <125807123+guyfoz77@users.noreply.github.com>
2023-10-31 10:29:23 +0000

    Basic CSS for layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import { userInputData } from "./data";
-import "./App.css";
-import { PersonalDetails } from "./components/PersonalDetails";
+import {
+  PersonalDetails,
+  PersonalDetailsCV,
+} from "./components/PersonalDetails";
 
 function App() {
   const [personalDetails, setPersonalDetails] = useState(
@@ -10,19 +12,24 @@ function App() {
   return (
     <div className="mainContainer">
       <aside className="userInput">
-        <div className="clearLoad">{/* todo */} clear/load</div>
+        <div className="clearLoad component">{/* todo */} clear/load</div>
         <PersonalDetails
-          className="personalDetails"
           name={personalDetails.name}
           email={personalDetails.email}
           phone={personalDetails.phone}
           address={personalDetails.address}
         />
-        <div className="education">{/* todo */}</div>
-        <div className="experience">{/* todo */}</div>
+        <div className="education component">{/* todo */}</div>
+        <div className="experience component">{/* todo */}</div>
       </aside>
       <main className="CV">
         <div className="personalDetailsCV">{/* todo */}</div>
+        <PersonalDetailsCV
+          name={personalDetails.name}
+          email={personalDetails.email}
+          phone={personalDetails.phone}
+          address={personalDetails.address}
+        />
         <div className="educationCV">{/* todo */}</div>
         <div className="professionalExperienceCV">{/* todo */}</div>
       </main>
--- src/components/PersonalDetails.jsx
+++ src/components/PersonalDetails.jsx
@@ -1,6 +1,6 @@
 export function PersonalDetails({ name, email, phone, address }) {
   return (
-    <>
+    <div className="personalDetails component">
       <h2>Personal Details</h2>
       <label htmlFor="name">Name:</label>
       <input type="text" id="name" value={name} />
@@ -10,11 +10,17 @@ export function PersonalDetails({ name, email, phone, address }) {
       <input type="phone" id="phone" value={phone} />
       <label htmlFor="address">Address:x</label>
       <input type="text" id="address" value={address} />
-    </>
+    </div>
   );
 }
 
 export function PersonalDetailsCV({ name, email, phone, address }) {
-  return 
+  return (
+    <div className="personalDetailsCV componentCV">
+      <h1>{name}</h1>
+      {email && (<div className="emailCV"><span className="material-symbols-sharp">mail</span><span> {email}</span></div>)}
+    </div>
+  )
   // To do
+
 }
