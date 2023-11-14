301a43efbb49cc80c435cd219ebd31fb5df30ab7
Guy <125807123+guyfoz77@users.noreply.github.com>
2023-11-14 18:26:15 +0000

    Add click and bin function for positions

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -47,6 +47,13 @@ export function ExperienceInputContainer({
     newCompanyList.push(newCompanyTemplate)
     handleExperienceDetailsChange(newCompanyList)
   }
+  function handlePositionDetailsChange(newPositions, companyKeyToEdit) {
+    const indexToEdit = experience.findIndex(
+      (company) => company.companyKey == companyKeyToEdit
+    )
+    newCompanyList[indexToEdit].positions = newPositions
+    handleExperienceDetailsChange(newCompanyList)
+  }
 
   return (
     <>
@@ -58,6 +65,7 @@ export function ExperienceInputContainer({
           onExperienceCompanyClick={onExperienceCompanyClick}
           onExperienceCompanyDelete={onExperienceCompanyDelete}
           onExperienceCompanyShowHide={onExperienceCompanyShowHide}
+          handlePositionDetailsChange={handlePositionDetailsChange}
         />
       </div>
       <div className='buttons'>
@@ -73,41 +81,47 @@ function ExperienceList({
   onExperienceCompanyClick,
   onExperienceCompanyDelete,
   onExperienceCompanyShowHide,
+  handlePositionDetailsChange,
 }) {
   return experience.map((company) => (
     <div key={company.companyKey}>
       <h3 onClick={() => onExperienceCompanyClick(company.companyKey)}>
         {company.company || 'Name me!'}
       </h3>
-      <span
+      <h3
         className='material-symbols-sharp delete'
         onClick={() => onExperienceCompanyDelete(company.companyKey)}
       >
         Delete
-      </span>
-      <span
+      </h3>
+      <h3
         className='material-symbols-sharp display'
         onClick={() => onExperienceCompanyShowHide(company.companyKey)}
       >
         {company.admin.displayOnCV ? 'visibility' : 'visibility_off'}
-      </span>
-      <span
+      </h3>
+      <h3
         className='material-symbols-sharp chevron'
         onClick={() => onExperienceCompanyClick(company.companyKey)}
       >
         {company.admin.editing ? 'expand_more' : 'expand_less'}
-      </span>
+      </h3>
       {company.admin.editing && (
         <CompanyInputs
           onExperienceDetailsChange={onExperienceDetailsChange}
           company={company}
+          handlePositionDetailsChange={handlePositionDetailsChange}
         />
       )}
     </div>
   ))
 }
 
-function CompanyInputs({ company, onExperienceDetailsChange }) {
+function CompanyInputs({
+  company,
+  onExperienceDetailsChange,
+  handlePositionDetailsChange,
+}) {
   return (
     <div className='educationDetailsInputContainer'>
       <label htmlFor='Company'>Company:</label>
@@ -120,18 +134,99 @@ function CompanyInputs({ company, onExperienceDetailsChange }) {
       <PositionList
         positions={company.positions}
         companyKey={company.companyKey}
+        handlePositionDetailsChange={handlePositionDetailsChange}
       />
     </div>
   )
 }
 
-function PositionList({ positions, companyKey }) {
-  //Next time, you need to add the same buttons to each of these divs as in the company list
-  //You need to make a function to handle the editing of a position
+function PositionList({ positions, companyKey, handlePositionDetailsChange }) {
+  //todo: make these buttons work (ive done the click and the bin)
+  //todo: make data show up on CV
+  let newPositionList = [...positions]
+
+  function onPositionDetailsChange(e, keyToEdit) {
+    //todo
+  }
+
+  function onExperiencePositionClick(keyToEdit) {
+    const indexToEdit = positions.findIndex(
+      (position) => position.positionKey == keyToEdit
+    )
+    newPositionList[indexToEdit].admin.editing =
+      !newPositionList[indexToEdit].admin.editing
+    handlePositionDetailsChange(newPositionList, companyKey)
+  }
+  function onExperiencePositionDelete(keyToDelete) {
+    const newPositionListDeleted = newPositionList.filter(
+      (position) => position.positionKey !== keyToDelete
+    )
+    handlePositionDetailsChange(newPositionListDeleted, companyKey)
+  }
 
   return positions.map((position) => (
     <div key={position.positionKey}>
-      <h4>{position.position}</h4>
+      <h4 onClick={() => onExperiencePositionClick(position.positionKey)}>
+        {position.position}
+      </h4>
+      <h4
+        className='material-symbols-sharp delete'
+        onClick={() => onExperiencePositionDelete(position.positionKey)}
+      >
+        Delete
+      </h4>
+      <h4 className='material-symbols-sharp display'>
+        {position.admin.displayOnCV ? 'visibility' : 'visibility_off'}
+      </h4>
+      <h4
+        className='material-symbols-sharp chevron'
+        onClick={() => onExperiencePositionClick(position.positionKey)}
+      >
+        {position.admin.editing ? 'expand_more' : 'expand_less'}
+      </h4>
+      {position.admin.editing && (
+        <PositionInputs
+          // onExperienceDetailsChange={onExperienceDetailsChange}
+          position={position}
+        />
+      )}
     </div>
   ))
+}
+
+//todo: make these change the stuff on the CV
+function PositionInputs({ onExperienceDetailsChange, position }) {
+  return (
+    <div className='positionDetailsInputsContainer'>
+      <label htmlFor='position'>Position:</label>
+      <input
+        type='text'
+        id='position'
+        defaultValue={position.position}
+        // onChange={}
+      />
+      <label htmlFor='start'>Start Date:</label>
+      <input
+        type='text'
+        id='start'
+        defaultValue={position.start}
+        // onChange={}
+      />
+      <label htmlFor='end'>End Date:</label>
+      <input
+        type='text'
+        id='end'
+        defaultValue={position.end}
+        // onChange={}
+      />
+      <label htmlFor='details'>Details:</label>
+      <textarea
+        rows={5}
+        type='text'
+        id='details'
+        defaultValue={position.details}
+        // onChange={}
+      />
+    </div>
+  )
 }
